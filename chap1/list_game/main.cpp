5ef16beadd40c4de89df6ac3aff19ff722cca329
zachary tyhacz <zactyh@gmail.com>
2019-01-13 19:23:59 -0500

    Continued working on first refresher mini project

--- chap1/list_game/main.cpp
+++ chap1/list_game/main.cpp
@@ -1,20 +1,50 @@
+/* Zachary Tyhacz 2018
+** chapter one refresher with c++, make a game asking if a given variable name
+** is a valid variable name 
+*/ 
+
 #include<iostream>
-#include<string>
+#include<cstring>
 
-public bool bAnswerIsValid;
-public int nTries = 0;
-public int arru16Answers[10];
-public string::string sQuestions[10] = {1111  
-
-
-bool IsCorrect(bool bValid){
-	return (bAnswerIsValid == bValid);
-}
+std::string answers[5] = {"n", "y", "y", "n", "n"};
+std::string questions[5] = {"int", "bIsCorrect", "my_name", "3_cars", "foo bar"};
 
 int main(){
-	bool bIsValid;
-	
-	cout << "-_VARIABLE_VALIDITY_-" << endl;
-	
-	return 0;
+   std::string input;
+	bool bQuit = true;
+	int i = 0;
+	int questions_count = (sizeof(questions)/sizeof(*questions));
+	int missed = 0;
+
+	do {
+		std::cout << "-_VARIABLE_VALIDITY_-" << std::endl;
+		/* Loop through questions and check each answer with the corresponding index in answers array */
+		for(i = 0; i < questions_count; i++){
+			std::cout << "Question #" << i << ":  Is '" << questions[i] << "' a valid variable name?" << std::endl << "Enter Y / N : ";
+			std::cin >> input;
+
+			/* turn input string to lower */
+			std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+			if(input == answers[i]){
+				std::cout << std::endl << "Correct!" << std::endl;
+			}else{
+				missed++;
+				std::cout << std::endl << "Incorrect..." << std::endl;
+			}
+		}
+		/* Show score */ 
+		std::cout << "Your score: " << (questions_count / (questions_count-missed));
+		/* Ask if they want to go through the quiz again */
+		std::cout << "Would you like to go through the quiz again to practice your variable naming skills?" << std::endl
+				  << "Y / N : ";
+		std::cin >> input;
+		std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+		
+		if(input == "y")
+			bQuit = false;
+
+	} while (!bQuit);
+
+	return 0; 
 }
