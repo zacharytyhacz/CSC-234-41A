e93603d6aaf62066c0ae7d5b6dfb5228abb7836b
zachary tyhacz <zactyh@gmail.com>
2019-03-17 20:10:43 -0400

    Added chapter 8

--- chapter8/chap8_1_and_2/main.cpp
+++ chapter8/chap8_1_and_2/main.cpp
@@ -1,31 +1,37 @@
+/* Zachary Tyhacz 2019 */
+/* Improve the critter program to have an unlisted menu choice that reveals exact values for the critter 
+and 
+change the critter program so that the critter is more expressive about its needs; hunger and boredom */
 #include<iostream>
 #include<string>
 #include<vector>
 
 class Critter{
 	public:
-		static int population = 0;
-		static int GetPopulation();
-		int GetHunger() const;
-		void SetHunger(int number);
 		void Stats();
 		void Greet();
-		Critter(int hunger = 0);
+		Critter(int hunger = 5, int boredom = 5);
 		void Talk();
 		void Eat();
 		void Play();
+		void PassTime();
 
 	private:
 		int Hunger;
 		int Boredom;
 };
 
+void Critter::PassTime(){
+	Hunger++;
+	Boredom++;
+}
+
 void Critter::Eat(){
-	Hunger--;
+	Hunger -= 2;
 	std::cout << "Yum yum!" << std::endl;
 }
 
-void Critter:Stats(){
+void Critter::Stats(){
 	std::cout << "Hunger: " << Hunger << std::endl;
 	std::cout << "Boredom: " << Boredom << std::endl;
 }
@@ -46,43 +52,25 @@ void Critter::Talk(){
 		std::cout << " I am not hungry." << std::endl;
 	else if(Hunger > 5)
 		std::cout << " I am kind of hungry." << std::endl;
-	else if(hunter > 3)
+	else if(Hunger > 3)
 		std::cout << " I'm hungry!" << std::endl;
 	else
 		std::cout << " I'm starving mister!" << std::endl;
 }
 
-int Critter::GetPopulation(){
-	return population;
-}
-
-void Critter::Critter(int hunger = 0){
+Critter::Critter(int hunger, int boredom){
 	std::cout << "A new critter has been born!" << std::endl;
 	Hunger = hunger;
-	Boredom = 3;
-	population++;
+	Boredom = boredom;
 }
 
-void Critter:Play(){
+void Critter::Play(){
 	std::cout << "ha ha this is fun!" << std::endl;
-	Boredom--;
-}
-
-int Critter::GetHunger(){
-	return Hunger;
-}
-
-void Critter::SetHunger(int number){
-	if(number < 0){
-		std::cout << "Invalid hunger level. Must be greater than 0." << std::endl;
-		return;
-	}
-
-	Hunger = number;
+	Boredom -= 2;
 }
 
 void Critter::Greet(){
-	std::cout << "Hello, I'm a critter. My hunger level is " << Hunger << << std::endl;
+	std::cout << "Hello, I'm a critter. My hunger level is " << Hunger << std::endl;
 }
 
 int main(){
@@ -90,13 +78,13 @@ int main(){
 	int choice;
 
 	do {
+		std::cout << "\n\n";
 		std::cout << "0 - Quit" << std::endl;
 		std::cout << "1 - Listen to your critter" << std::endl;
 		std::cout << "2 - Feed your critter" << std::endl;
 		std::cout << "3 - Play with your critter" << std::endl;
-		std::cout << "4 - View Stats" << std::endl;
 
-		std::cout << "\n\n Select a choice: ";
+		std::cout << "\n Select a choice: ";
 		std::cin >> choice;
 		std::cout << "\n\n";
 
@@ -112,9 +100,14 @@ int main(){
 				break;
 			case 3:
 				my_pet.Play();
+				break;
+			case 4:
+				my_pet.Stats();
+				break;
 			default:
 				std::cout << choice << " is an invalid choice." << std::endl;
 		}
+		my_pet.PassTime();
 	} while(choice != 0); 
 	return 0;
 }
