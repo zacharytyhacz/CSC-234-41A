9172f3809554af1a5147d5af50880ad2cae238c4
zachary tyhacz <zactyh@gmail.com>
2019-04-24 21:38:50 -0400

    added comments to exercise 3 why it is a bad program

--- chapter9/exercise_3/main.cpp
+++ chapter9/exercise_3/main.cpp
@@ -2,9 +2,14 @@
 
 int main(){
 	int* pScore = new int;
+	std::cout << *pScore << std::endl;
 	*pScore = 500;
+	std::cout << *pScore << std::endl;
 	pScore = new int(1000);
+	std::cout << *pScore << std::endl;
 	delete pScore;
-	pScore = 0;
+	std::cout << *pScore << std::endl;
+	pScore = 0;		// <<< since pScore has been deleted on line 10, this is null and cannot be set as if pScore was never initialized.
+	std::cout << *pScore << std::endl;
 	return 0;
 }
